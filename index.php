457516dc198fbf3dbf04c9632516b38a12ddb66a
Keitsun <ashirogimoto109@gmail.com>
2020-10-04 15:18:18 +0200

    first commit SENSEI

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SENSEI</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .fourth-section{
+            width: 100%;
+            height: 100vh;
+            background-color: #fec303;
+            text-align:center;
+            padding:30px 30px;
+        }
+
+
+        .fourth-section .meet{
+            color: #131419;
+            font-size:40px;
+            font-weight: 700;
+        }
+
+        .fourth-section .where{
+            color: #131419;
+            font-size:50px;
+            font-weight: 500;
+        }
+
+        .fourth-section p{
+            padding:10px;
+            color: #131419;
+            font-size:18px;
+            font-weight: 300;
+        }
+        .fourth-section .card-img-container{
+            display:flex;
+            justify-content:space-between;
+            align-items:center;
+            padding-top:10px;
+        }
+
+        .fourth-section .card-img-container .card-img{
+            width: 30%;
+            height:50vh;
+            position: relative;
+        }
+
+        .fourth-section .card-img-container .card-img .member-img{
+            width:100%;
+            height:55vh;
+            background-repeat: no-repeat;
+            background-size: cover;
+            border-radius: 50%;
+        }
+
+        
+
+        .fourth-section .card-img-container .card-img .m-img1{
+            background-image:url(img/15f2f3f72ce4392e5165f427dee973fa.jpg);
+        }
+        .fourth-section .card-img-container .card-img .m-img2{
+            background-image:url(img/joel_harper_headshot_color_llr.jpg);
+        }
+        .fourth-section .card-img-container .card-img .m-img3{
+            background-image:url(img/f640611135e3258f8ca3d97ac11f4430.jpg);
+        }
+
+        .fourth-section .card-img-container .card-img .overlay{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width:100%;
+            height:55vh;
+            background: rgba(24, 21, 4, 0.3);
+            border-radius: 50%;
+        }
+
+
+        .fourth-section .card-img-container .card-img .overlay:hover{
+            background: rgba(231, 198, 33, .7);
+            cursor:pointer;
+        }
+
+        .fourth-section .card-img-container .card-img .coach-name{
+            padding-top:1px;
+            font-size:28px;
+            font-weight:600;
+            color:#131419;
+        }
+
+        .fourth-section .card-img-container .card-img .coach-title{
+            font-size:20px;
+            font-weight:500;
+            color:#131419;
+        }
+    </style>
+</head>
+
+
+
+<body>
+    <div class="top-section">
+        <div class="header">
+            <div class="logo"><a href="#"><span>SEN</span>SEI</a></div>
+
+            <div class="nav-links">
+                <a href="#">ABOUT US</a>
+                <a href="#">PARTENERS</a>
+                <a href="#">BLOG</a>
+                <a href="#">CONTACTS</a>
+            </div>
+
+            <div class="menu">
+                <a href="#"><i class="fa fa-list"></i>MENU</a>
+            </div>
+
+            <div class="social-links">
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="second-section">
+        <!-- <img style="width: 100%;" class="allsports-thumb" src="img/AdobeStock_193128467_1.jpg" alt=""> -->
+        <div class="img-filter"></div>
+        <img style="width:50.8%; height:100vh; position:absolute; left:50px" class="player-regby" src="img/player-regby-png.png" alt="">
+        <div class="titles">
+            <h3 style=""><span style="color:#ecc113">WELCOME</span> TO THE</h3>
+            <h2>ULTIMATE FIGHTING</h2>
+            <h1>SPORTS ARTS CLUB</h1>
+            <p>Can there be anything better than being able to set up a sports spirit?</p>
+        </div>
+        <a href="#" class="btn-sign-up">
+            <h4>START SIGNING UP</h4>
+            <i style="font-size:30px; font-weight:700;" class="fa fa-angle-right"></i>
+        </a>
+    </div>
+
+    <div class="third-section">
+        <div class="player-profile">
+            <div class="img1"></div>
+            <div class="overlay"></div>
+            <a class="sport-genre">
+                <h4>BASKETBALL</h4>
+            </a>
+        </div>
+        <div class="player-profile">
+            <div class="img2"></div>
+            <div class="overlay"></div>
+            <a class="sport-genre">
+                <h4>BASEBALL</h4>
+            </a>
+        </div>
+        <div class="player-profile">
+            <div class="img3"></div>
+            <div class="overlay"></div>
+            <a class="sport-genre">
+                <h4>AMERICAN FOOTBALL</h4>
+            </a>
+        </div>
+        <div class="player-profile">
+            <div class="img4"></div>
+            <div class="overlay"></div>
+            <a class="sport-genre">
+                <h4 class="sport-genre-h4">FOOTBALL</h4>
+            </a>
+        </div>
+    </div>
+
+    <div class="fourth-section">
+        <div class="meet">Meet The Team</div>
+        <div class="where">Where Everyone is a Fighter!</div>
+        <p>There’s no better friend than a fistful of power and knowledge!</p>
+        <div class="card-img-container">
+            <div class="card-img">
+                <div class="member-img m-img1"></div>
+                <div class="overlay"></div>
+                <div class="coach-name">Hans Breuter</div>
+                <div class="coach-title">Owner/Head</div>
+            </div>
+            <div class="card-img">
+                <div class="member-img m-img2"></div>
+                <div class="overlay"></div>
+                <div class="coach-name">Dirk Merit</div>
+                <div class="coach-title">Instructor</div>
+            </div>
+            <div class="card-img">
+                <div class="member-img m-img3"></div>
+                <div class="overlay"></div>
+                <div class="coach-name">Mary Seficient</div>
+                <div class="coach-title">Instructor</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
